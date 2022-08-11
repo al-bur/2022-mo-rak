e371f50d7e5a95755b66dd2cfc6201883326f910
al-bur <64825713+al-bur@users.noreply.github.com>
2022-08-11 20:16:08 +0900

    chore: react-query 초기 설정을 위한 컴포넌트 생성

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,18 +2,23 @@ import { ThemeProvider } from '@emotion/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
 import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
